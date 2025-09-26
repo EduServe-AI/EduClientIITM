a6613b9d3b070506206f3f92b87d4b8b8168de12
Jayanth <annajayanth27@gmail.com>
2025-09-26 20:49:39 +0530

    comments added

--- app/onboarding/instructor/page.tsx
+++ app/onboarding/instructor/page.tsx
@@ -255,6 +255,7 @@ export default function instructorOnboarding() {
 
       // router.push('/dashboard/instructor')
       router.push('/verification')
+      //here we redirect to verification
     } catch (error) {
       if (error instanceof Error) {
         console.error('Error has been occurred', error)
